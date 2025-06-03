93bccd4ad85572a8606d02ceb71a8f898997fe8b
ingleshivam <ingleshivam@gmail.com>
2025-06-03 23:27:04 +0530

    03-06-2025

--- src/app/api/addRecipe/route.ts
+++ src/app/api/addRecipe/route.ts
@@ -5,6 +5,32 @@ export  async function POST(request:Request){
     try {
         const data = await request.json();
         console.log("Data : ", data);
+        const response = await prisma?.recipe.create({
+            data : {
+                title : data.recipeTitle,
+                description : data.recipeDescription,
+                cookingTime : data.cookTime,
+                servingSize : data.servings,
+                categoryId : parseInt(data.category),
+                images : {
+                    create :{
+                        imageUrl : data.imageUrl,
+                        description : "NA"
+                    }
+                },
+                ingredients :{
+                    create : data.ingredients.map((ing : any)=> ({
+                        quantity : ing.amount,
+                        name : ing.item
+                    }))
+                },
+                instructions : {
+                    create : data.instructions.map((ing:any)=> ({
+                        
+                    }))
+                }
+            }
+        })
         return NextResponse.json({status : 200,  message :{success : "Data is stored successfully !", error : ""}})
     } catch (error) {
         if(error instanceof Error){
